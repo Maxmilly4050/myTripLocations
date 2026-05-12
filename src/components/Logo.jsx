6a52eac2700ea228a379995687522b19bf0d36f4
Maxmilly4050 <maxsteel4050@gmail.com>
2026-05-12 22:14:29 +0300

    Adding of the map component to the app layout page

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,11 @@ import styles from "./Logo.module.css";
 function Logo() {
   return (
     <Link to="/" className={styles.logo}>
-      <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
+      <img
+        src="/myTrip.png"
+        alt="WorldWise logo"
+        className={styles.logoImage}
+      />
     </Link>
   );
 }
