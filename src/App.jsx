90ae73749510e2acd272328bd103baaedc201430
Maxmilly4050 <maxsteel4050@gmail.com>
2026-05-17 19:43:41 +0300

    feat: add index route to the /app url

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
           <Route path='/app' element={<AppLayout />}>
+          <Route index element={<h1>Welcome to the app</h1>} />
             <Route path='cities' element={<h1>Cities</h1>} />
             <Route path='countries' element={<h1>Countries</h1>} />
             <Route path='form' element={<h1>Form</h1>} />
